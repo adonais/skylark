723038bd03254184055c4118ee02cf6f2b04f9ab
adonais <lihualove1@163.com>
2023-07-28 00:44:33 +0800

    Merge branch '3.x' into release

--- src/eu_about.c
+++ src/eu_about.c
@@ -3149,9 +3149,9 @@ static const uint8_t wc_buffer[29409] = {
 static time_t
 get_compiler_time(char const *date_, char const *time_)
 {
-    char s_month[5];
     int year;
     struct tm t;
+    char s_month[5];
     const char month_names[] = "JanFebMarAprMayJunJulAugSepOctNovDec";
     sscanf(date_, "%s %d %d", s_month, &t.tm_mday, &year);
     sscanf(time_, "%2d %*c %2d %*c %2d", &t.tm_hour, &t.tm_min, &t.tm_sec);
@@ -3160,7 +3160,7 @@ get_compiler_time(char const *date_, char const *time_)
     t.tm_mon = (int)((strstr(month_names, s_month) - month_names) / 3);
     t.tm_year = year - 1900;
     t.tm_isdst = -1;
-    return mktime(&t);
+    return _mkgmtime(&t);
 }
 
 /* End of HyperLink URL */
@@ -3660,6 +3660,16 @@ on_about_dialog(void)
 uint64_t
 on_about_build_id(void)
 {
+#ifndef ACTIONS_BUILDING
+    int zone = 0;
+    TIME_ZONE_INFORMATION tzi;
+    GetTimeZoneInformation(&tzi);
+    if ((zone = (int)tzi.Bias/60))
+    {
+        printf("If it is a local compilation, add this time offset, zone = %d\n", zone);
+        return (zone * 3600 + get_compiler_time(__DATE__, __TIME__));
+    }
+#endif
     return get_compiler_time(__DATE__, __TIME__);
 }
 
