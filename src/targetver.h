b7fef0334c750d50b64626d15aa5036f465dd0b3
adonais <lihualove1@163.com>
2023-07-30 15:46:28 +0800

    Bump version to 3.0.14

--- src/targetver.h
+++ src/targetver.h
@@ -30,7 +30,7 @@
 // 定义版本信息
 #define __EU_INFO_VERSION 3
 #define __EU_INFO_VERSION_MINOR 0
-#define __EU_INFO_VERSION_PATCHLEVEL 13
+#define __EU_INFO_VERSION_PATCHLEVEL 14
 #define __ORIGINAL_NAME TEXT("skylark.exe")
 
 #if !__EU_INFO_VERSION_PATCHLEVEL
